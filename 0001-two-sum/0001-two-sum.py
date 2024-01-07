f64190478b5ca3c991aa9fcbabe6eefc0cd699ae
Arthur <123634659+arthur-ru@users.noreply.github.com>
2024-01-07 22:44:30 +0100

    Time: 38 ms (80.12%), Space: 14.1 MB (69.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,9 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # We use a one way pass in the list: O(n)
+        # Create a hashmap to improve efficiency
         n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if nums[i] + nums[j] == target:
-                    return i,j
-        return False
+        h = {}
+        for i in range(n):
+            # Instead of searching for two numbers, we are now searching for one
+            ans = target - nums[i]
+            if ans in h:
+                return i,h[ans]
+            # If we can't find an addition that fits, we add the number to the hashmap
+            h[nums[i]] = i
